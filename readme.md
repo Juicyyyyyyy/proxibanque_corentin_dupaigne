e70bcb56107f7db05d842082bef51b089fbd7938
corentin.dupaigne <declan.lowee@gmail.com>
2025-11-20 11:36:36 +0100

    init project

---
+++ readme.md
@@ -0,0 +1,27 @@
+### Objets
+- Client
+* name
+* address
+* postal_code
+* hometown
+* phone_number
+
+- Employee
+* agency_id
+* name
+* email
+* password
+
+- Agency
+* owner_id
+* name
+
+- BankingAccount
+* owner_id
+* account_number
+* quantity
+* opened_date
+
+- BankingCard
+* account_id
+* card_type
---
+++ src/main/java/org/formation/proxibanque_corentin_dupaigne/ProxibanqueCorentinDupaigneApplication.java
@@ -0,0 +1,13 @@
+package org.formation.proxibanque_corentin_dupaigne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxibanqueCorentinDupaigneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProxibanqueCorentinDupaigneApplication.class, args);
+    }
+
+}
